995145911bb2abc27e7b06265f240b5adfdc9784
pjilipu11 <78666193+pjilipu11@users.noreply.github.com>
2021-10-11 15:22:44 -0300

    SellerService and basic FindAll

--- SallesWebMvc/Controllers/SallersController.cs
+++ SallesWebMvc/Controllers/SallersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SallesWebMvc.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,16 @@ namespace SallesWebMvc.Controllers
 {
     public class SallersController : Controller
     {
+        private readonly SellerService _sellerService;
+        public SallersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerService.FindAll();
+            return View(list);
         }
     }
 }
